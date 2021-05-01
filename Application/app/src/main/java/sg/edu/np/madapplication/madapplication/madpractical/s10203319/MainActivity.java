1f6f14e66390bcaadbbc6fdc4c1f76ebe4ce3552
TheBetterSamuel <s10203319@connect.np.edu.sg>
2021-05-02 00:20:30 +0800

    Added Application folder

---
+++ Application/app/src/main/java/sg/edu/np/madapplication/madapplication/madpractical/s10203319/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.np.madapplication.madapplication.madpractical.s10203319;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
